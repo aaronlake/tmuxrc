bbe30745f353d6d2f9fcf1969d8e3f6d6c196d94
Aaron Lake <aaron.lake@gmail.com>
2012-12-18 22:26:59 -0500

    Fix problem with Linux tmux [exit] with no reattach-to-user-space.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ function die()
     exit 1
 }
 
-for i in $HOME/.tmux $HOME/.tmux.conf; do
+for i in $HOME/.tmux $HOME/.tmux.conf $HOME/.tmux-osx.conf; do
   if [[ ( -e $i ) || ( -h $i ) ]]; then
     echo "${i} has been renamed to ${i}.old"
     mv "${i}" "${i}.old" || die "Could not move ${i} to ${i}.old"
@@ -17,5 +17,6 @@ git clone --recursive git://github.com/aaronlake/tmuxrc.git $HOME/.tmux \
     || die "Could not clone the repository to ${HOME}/.tmux"
 
 ln -s $HOME/.tmux/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/.tmux/tmux-osx.conf $HOME/.tmux-osx.conf
 
 echo "Install successfully."
